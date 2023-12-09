7774888eb7d4b781beb965d4e5b18fa45d8bb7d0
Dhammarin <ueang@Dhammarins-MacBook-Air.local>
2023-12-09 19:09:06 +0700

    feature(exercise): add completed exercise1-2 (equality of values)

--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,10 @@
 // Exercise #2: Loose Equality
 // Start commenting here
-0 == false;
-null == undefined;
-10 == "10";
-[10] == 10;
-[1,2] == "1,2";
+0 == false; //true
+null == undefined; //false
+10 == "10"; //true
+[10] == 10; //true
+[1,2] == "1,2"; //true
+
+console.log([10] == 10)
+console.log([1,2] == "1,2"); //true
